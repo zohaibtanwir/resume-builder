56eec6a8953a62357552806cea9b6532459a338a
Akhilesh Das <akhilesh-das@hotmail.com>
2025-08-10 10:59:28 +0530

    chore: set up mantime UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
 import App from "./App.tsx";
 
 const ROOT_ELEMENT = document.getElementById("root") as HTMLElement;
 
 createRoot(ROOT_ELEMENT).render(
 	<StrictMode>
-		<App />
+		<MantineProvider>
+			<App />
+		</MantineProvider>
 	</StrictMode>
 );
