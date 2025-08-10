a0feb976d8f635c08e2f5f9bac544dc36cdf98f1
Akhilesh Das <akhilesh-das@hotmail.com>
2025-08-10 17:10:45 +0530

    feat: scroll area for form

--- src/app/pages/builder/Builder.tsx
+++ src/app/pages/builder/Builder.tsx
@@ -9,6 +9,8 @@ import Underline from "@tiptap/extension-underline";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+import styles from "./Builder.module.scss";
+
 function _Form() {
 	const DUMMY_CONTENT =
 		'<h2 style="text-align: center;">Welcome to Mantine rich text editor</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
@@ -27,7 +29,7 @@ function _Form() {
 	});
 
 	return (
-		<>
+		<div className={styles.form}>
 			<Title order={3}>Edit Resume</Title>
 			<Title order={5}>Personal Details</Title>
 			<Grid>
@@ -154,7 +156,7 @@ function _Form() {
 					</Input.Wrapper>
 				</Grid.Col>
 			</Grid>
-		</>
+		</div>
 	);
 }
 
